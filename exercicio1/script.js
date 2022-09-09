f660d6bd15b6c8c501036ca4f7356284d0689bed
heyalearaujo <alexandrearza@gmail.com>
2022-09-09 09:54:28 -0300

    Resolução exercicio 1 - Condicionais2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = Number(prompt("Insira um número"))
 
+if (num % 2 === 0){
+    console.log("Este número é divisivel por 2")
+    if (num % 3 === 0){
+        console.log("Este número é divisivel por 3")
+
+    }else {
+        console.log("Não é divisivel por 3")
+    }
+}else if (num % 3 === 0){
+    console.log("Não é divisivel por 2")
+}
+
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log("Este número é divisivel por 2 e 3!")
+}
+
+///Não deu certo.
